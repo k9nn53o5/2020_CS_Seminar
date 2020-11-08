77b9c2b8cd3264c020aa10ae81044a4592735ae3
unknown <k9nn53o5>
2020-11-08 14:39:36 +0800

    commit123456789

--- model/Order_goods.js
+++ model/Order_goods.js
@@ -13,11 +13,11 @@ function Order_goodsDao(){
 }
 
 Order_goodsDao.prototype.newAOrder_goods = function(orderId,dishId,number,price,status,create_time,update_time){
-    var sqlCode = "INSERT INTO order_goods (orderId,dishId,number,price,status,create_time,update_time)\
+    var sqlCode = "INSERT INTO order_goods (og_orderId,og_dishId,og_number,og_price,og_status,og_create_time,og_update_time)\
                    VALUES ?";
     
     MySQL
-        .query(sqlCode,[ [orderId],[dishId],[number],[price],[status],[create_time],[update_time] ],function(err){
+        .query(sqlCode,[ [orderId],[dishId],[number],[price],[null],[create_time],[update_time] ],function(err){
             if(err)
                 throw err;
         });
@@ -26,19 +26,17 @@ Order_goodsDao.prototype.newAOrder_goods = function(orderId,dishId,number,price,
 Order_goodsDao.prototype.findOrder_goodsBy_orderId = function(orderId,callback){
     var sqlCode = 'SELECT *\
                    FROM order_goods\
-                   WHERE orderId = ?';
-
-                   
+                   WHERE og_orderId = ?';
 
     MySQL
-        .query(sqlCode,[orderId],function(err,rows){
+        .query(sqlCode,[[orderId]],function(err,rows){
             if(err)
                     throw err;
             var results = [];
             rows.forEach(function(element) {
-                results.push(new Order_goods(element.id,element.orderId,element.dishId,
-                    element.number,element.price,element.status,
-                    element.create_time,element.update_time));
+                results.push(new Order_goods(element.og_id,element.og_orderId,element.og_dishId,
+                    element.og_number,element.og_price,null,
+                    element.og_create_time,element.og_update_time));
             });
             callback(results);
         });
@@ -48,7 +46,7 @@ Order_goodsDao.prototype.findOrder_goodsBy_orderId = function(orderId,callback){
 Order_goodsDao.prototype.dishIdCanNotBeDuplicateInOrder_goods = function(orderId,dishId,callback){
     var sqlCode = "SELECT *\
                    FROM order_goods\
-                   WHERE orderId = ? AND dishId = ? AND status != 'Finish' ";
+                   WHERE og_orderId = ? AND og_dishId = ? AND og_status != 'Finish' ";
     MySQL
     .query(sqlCode,[ [orderId],[dishId] ],function(err,rows){
             if(err)
